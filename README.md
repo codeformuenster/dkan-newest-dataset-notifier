d716b3fce5beb11df8f2aedd1904dfb9302a4ccd
Gerald Pape <gerald@giantswarm.io>
2020-04-06 18:35:42 +0200

    add flag =for specifically enabling tweeting & smart data file path assumptions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # DKAN New Dataset Tweeter
 
+Sends automated tweets containing newly created datasets in the [open data portal of the city of Münster](https://opendata.stadt-muenster.de/).
+
 ## Obtaining the consumerKey, consumerSecret, accessToken, accessSecret values
 
 You have to register an app at developer.twitter.com
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path"
 	"time"
 
 	"github.com/codeformuenster/dkan-newest-dataset-notifier/datasets"
@@ -11,13 +13,13 @@ import (
 
 const (
 	defaultDataJSONURL = "https://opendata.stadt-muenster.de/data.json"
-	defaultLocalPath   = "./data.json"
+	defaultLocalPath   = ""
 )
 
 var (
-	dataJSONURL                                            string
-	localPath                                              string
+	dataJSONURL, localPath                                 string
 	consumerKey, consumerSecret, accessToken, accessSecret string
+	enableTweeter                                          bool
 )
 
 // How this works (at least in my head)
@@ -31,6 +33,7 @@ func main() {
 	flag.StringVar(&consumerSecret, "consumer-secret", "", "twitter oauth1 consumerSecret")
 	flag.StringVar(&accessToken, "access-token", "", "twitter oauth1 accessToken")
 	flag.StringVar(&accessSecret, "access-secret", "", "twitter oauth1 accessSecret")
+	flag.BoolVar(&enableTweeter, "enable-tweeter", false, "enable the creation of tweets")
 
 	flag.StringVar(&dataJSONURL, "url", defaultDataJSONURL, "url of the remote json file containing dkan datasets")
 	flag.StringVar(&localPath, "local-path", defaultLocalPath, "path to local json file for comparison")
@@ -38,6 +41,15 @@ func main() {
 	flag.Parse()
 
 	t, tweeterAvailable := setupTweeter()
+
+	if tweeterAvailable == false {
+		fmt.Println("disabling tweeter, no tweets will be created")
+	}
+
+	if localPath == "" {
+		localPath = makeDataPath(time.Now().Add(-24 * time.Hour))
+		fmt.Printf("empty local-path flag, assuming path %s\n", localPath)
+	}
 
 	prevDatasets, err := datasets.FromPath(localPath)
 	if err != nil {
@@ -56,8 +68,12 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		text := fmt.Sprintf("Datensatz \"%s\" nun verfügbar im Open-Data-Portal Münster %s #OpenData #Münster", m.Title, url)
-		fmt.Printf("%d %s\n", len(text), text)
+		text := fmt.Sprintf("\"%s\" ist nun als Datensatz im Open-Data-Portal Münster verfügbar %s #OpenData #Münster", m.Title, url)
+		textLen := len(text)
+		if textLen > 280 {
+			fmt.Print("!!!! ")
+		}
+		fmt.Printf("%d %s\n", textLen, text)
 
 		if tweeterAvailable == true {
 			err = t.SendTweet(text)
@@ -68,7 +84,7 @@ func main() {
 		}
 	}
 
-	err = currDatasets.Save(fmt.Sprintf("./data-%s.json", time.Now().Format("2006-01-02")))
+	err = currDatasets.Save(makeDataPath(time.Now()))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -76,7 +92,11 @@ func main() {
 }
 
 func setupTweeter() (tweeter.Tweeter, bool) {
-	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+	if enableTweeter == false ||
+		consumerKey == "" ||
+		consumerSecret == "" ||
+		accessToken == "" ||
+		accessSecret == "" {
 		return tweeter.Tweeter{}, false
 	}
 
@@ -87,4 +107,14 @@ func setupTweeter() (tweeter.Tweeter, bool) {
 	}
 
 	return t, true
+}
+
+func makeDataPath(date time.Time) string {
+	filename := fmt.Sprintf("data-%s.json", date.Format("2006-01-02"))
+	dir, err := os.Getwd()
+	if err != nil {
+		return "./" + filename
+	}
+
+	return path.Join(dir, filename)
 }
